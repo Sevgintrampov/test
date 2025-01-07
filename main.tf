531e408924335fcc02fc895eb9992750eeed7873
Sevgintrampov <168405248+Sevgintrampov@users.noreply.github.com>
2025-01-07 10:45:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,7 @@ variable "tfc_workspace_name" {
   type        = string
   description = "The name of the workspace that you'd like to create and connect to AWS"
 }
+variable "tfc_workspace_nam" {
+  type        = string
+  description = "The name of the workspace that you'd like to create and connect to AWS"
+}
