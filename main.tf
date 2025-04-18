1935442f5411a8bdc2fc8c519ec8a61a85da9663
Sevgintrampov <168405248+Sevgintrampov@users.noreply.github.com>
2025-04-18 15:37:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 resource "null_resource" "cluster" {
   }
 
-variable "tfc_workspace_name" {
-  type        = string
-  description = "The name of the workspace that you'd like to create and connect to AWS"
-}
-variable "tfc_workspace_nam" {
-  type        = string
-  description = "The name of the workspace that you'd like to create and connect to AWS"
+output "timeouts" {
+  value = null
 }
